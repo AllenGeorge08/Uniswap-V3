36b6e87167dfaa532031f3e26d58a3f0b0b5ab7d
Allen <allengeorge2400@gmail.com>
2025-07-30 11:19:00 +0000

    Added getters

--- src/lib/Position.sol
+++ src/lib/Position.sol
@@ -13,4 +13,15 @@ library Position {
         uint128 liquidityAfter = liquidityBefore + liquidityDelta;
         self.liquidity = liquidityAfter;
     }
+
+    function get(
+        mapping (bytes32 => Info) storage self,
+        address owner,
+        int24 lowerTick,
+        int24 upperTick
+    ) internal view returns (Info storage position){
+        position = self[
+            keccak256(abi.encodePacked(owner,lowerTick,upperTick))
+        ];
+    }
 }
